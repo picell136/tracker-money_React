ea925bdbfd3c21ae205a754584e9b116a41c613b
Andrey <susluk93@mail.ru>
2026-04-13 18:51:29 +0300

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tracker-money_React/',
+  base: '/tracker-money_React',
   plugins: [react()],
 })
